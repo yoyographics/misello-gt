140f7516ce6b3716937139b62851744fc270b45e
YOYO Deploy <deploy@yoyographics.com>
2026-05-26 10:36:08 -0600

    fix: importa AuthJwtModule en CustomersModule

--- backend/src/customers/customers.module.ts
+++ backend/src/customers/customers.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CustomersController } from './customers.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthJwtModule } from '../auth/auth-jwt.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthJwtModule],
   controllers: [CustomersController],
   providers: [CustomersService],
   exports: [CustomersService],
