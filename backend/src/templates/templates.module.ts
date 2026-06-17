d6091a7d12b49c1e1f1342c89f463ef5c8f771cc
YOYO Deploy <deploy@yoyographics.com>
2026-06-17 17:30:54 -0600

    fix(templates): importar AuthJwtModule y AuthModule para JwtAdminGuard

--- backend/src/templates/templates.module.ts
+++ backend/src/templates/templates.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthJwtModule } from '../auth/auth-jwt.module';
+import { AuthModule } from '../auth/auth.module';
 import { TemplatesService } from './templates.service';
 import { TemplatesController } from './templates.controller';
 
 @Module({
+  imports: [AuthJwtModule, AuthModule],
   controllers: [TemplatesController],
   providers: [TemplatesService],
   exports: [TemplatesService],
